04b31ebce7176929ea0fd20f2815d0d057bc3886
Blessing Uche <blessinguche@Blessings-MacBook-Air.local>
2023-01-09 15:58:24 +0000

    cool

--- nc-games/src/components/Reviews.js
+++ nc-games/src/components/Reviews.js
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
-import styles from "../styles/Nav.module.css";
+import styles from "../styles/Reviews.module.css";
 
 export default function Reviews() {
    return (
      <div >
-      
+
      </div>
    );
  }
